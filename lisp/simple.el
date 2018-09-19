c97c771f58f9484d339844023d81176595d16b3e
Ernesto Alfonso <erjoalgo@gmail.com>
2018-09-19 10:22:47 -0700

    use point instead of compilation-current-error

--- lisp/simple.el
+++ lisp/simple.el
@@ -445,7 +445,7 @@ buffer causes automatic display of the corresponding source code location."
       (when next-error-message-highlight-overlay
         (delete-overlay next-error-message-highlight-overlay))
       (save-excursion
-        (goto-char compilation-current-error)
+        (goto-char (point))
         (let ((ol (make-overlay (line-beginning-position) (line-end-position))))
           ;; do not override region highlighting
           (overlay-put ol 'priority -50)
